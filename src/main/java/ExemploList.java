925d061069cbc85cad80fe2e761e983fc814444d
Davidson <dehpacifico@gmail.com>
2023-12-28 19:59:09 -0300

    conhecendo métodos da classe List

--- src/main/java/ExemploList.java
+++ src/main/java/ExemploList.java
@@ -1,7 +1,5 @@
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class ExemploList {
     public static void main(String[] args) {
@@ -41,7 +39,43 @@ public class ExemploList {
 //        System.out.println("Exiba todas as notas na ordem em que foram informados: " );
 //        for( Double nota: notas) System.out.println(nota);
 
+        System.out.println("Exiba a terceira nota adicionada: " + notas.get(2));
 
+        System.out.println("Exiba a menor nota: " + Collections.min(notas));
+
+        System.out.println("Exiba a maior nota: " + Collections.max(notas));
+
+
+        Iterator<Double> iterator = notas.iterator();
+        Double soma = 0d;
+        while(iterator.hasNext()){
+            Double next = iterator.next();
+            soma += next;
+        }
+        System.out.println("Exiba a soma dos valores: " + soma);
+
+        System.out.println("Exiba a média das notas: " + (soma/notas.size()));
+
+        System.out.println("Remova a nota 0: ");
+        notas.remove(0d);
+        System.out.println(notas);
+
+        System.out.println("Remova a nota da posicao 0: ");
+        notas.remove(0);
+        System.out.println(notas);
+
+        System.out.println("Remova as notas menores que 7 e exiba a lista: ");
+        Iterator<Double> iterator1 = notas.iterator();
+        while(iterator1.hasNext()){
+            Double next = iterator1.next();
+            if(next < 7) iterator1.remove();
+        }
+        System.out.println(notas);
+
+        notas.clear();
+        System.out.println("Apague toda a lista: " + notas);
+
+        System.out.println("Confia se a lista está vazia: " + notas.isEmpty());
 
     }
 
